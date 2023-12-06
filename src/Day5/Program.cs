b1a5d3183650c1a1fba25909c8d7082594b88461
Michael Farrelly <mikefarrelly@gmail.com>
2023-12-05 21:02:08 -0700

    day5.part2: passes but doesnt complete full

--- src/Day5/Program.cs
+++ src/Day5/Program.cs
@@ -10,6 +10,13 @@ class Program
 
         Console.WriteLine($"Found {data.Count()} lines");
 
+        Part1(data);
+
+        Part2(data);
+    }
+
+    public static void Part1(string[] data)
+    {
         var seedLine = data[0];
         var seedValues = seedLine
             .Replace("seeds: ", "")
@@ -78,6 +85,116 @@ class Program
         }).OrderBy(item => item.nextInput);
 
         Console.WriteLine($"Lowest is {result.First().nextInput}");
+
+
+        // part 2
+
+    }
+
+    public static void Part2(string[] data)
+    {
+        var seedLine = data[0];
+        var seedValueInput = seedLine
+            .Replace("seeds: ", "")
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => long.Parse(s))
+            .ToArray();
+
+        var seedValues = new List<(long start, long range)>();
+        for (var s = 0; s < seedValueInput.Count(); s += 2)
+        {
+            seedValues.Add((seedValueInput[s], seedValueInput[s + 1]));
+        }
+
+        var dataLines = data[2..];
+
+        var sectionName = string.Empty;
+
+        var sectionDatas = new List<SectionData>();
+        SectionData? activeSectionData = null;
+
+        foreach (var line in dataLines)
+        {
+            // determine if new section or not.
+            if (ValidSectionName(line))
+            {
+                if (activeSectionData != null)
+                {
+                    // add to sectionDatas
+                    sectionDatas.Add(activeSectionData);
+                }
+                sectionName = line.Split(":")[0].Trim();
+                activeSectionData = new SectionData() { Name = sectionName };
+            }
+            else if (!string.IsNullOrWhiteSpace(line) && activeSectionData != null)
+            {
+                // collect values
+                var parts = line.Split(" ");
+                activeSectionData.Rows.Add(new SectionRow()
+                {
+                    DestRangeStart = long.Parse(parts[0]),
+                    SourceRangeStart = long.Parse(parts[1]),
+                    RangeLength = long.Parse(parts[2])
+                });
+            }
+        }
+        if (activeSectionData != null)
+        {
+            // add to sectionDatas
+            sectionDatas.Add(activeSectionData);
+        }
+
+
+        var result = seedValues.Select(sv =>
+        {
+            // foreach (var seed in seedValues)
+            // {
+
+            // foreach (var seedRange in Enumerable.Range(sv.start, sv.range))
+            // {
+
+            // var resultInner = new List<(long input, long nextInput)>();
+            Console.WriteLine($"Part2: {sv.start}+{sv.range}: start");
+            long? lowest = null;
+            for (var s = sv.start; s < sv.start + sv.range; s += 1)
+            {
+                var input = s;
+                long nextInput = s;
+                foreach (var sd in sectionDatas)
+                {
+                    var m = sd.FindValue(nextInput);
+                    if (m.match)
+                    {
+                        // Console.WriteLine($"Found in {sd.Name} for {input}: {nextInput}");
+                        nextInput = m.value ?? -1;
+                    }
+
+                }
+                // Console.WriteLine($"Found for {input}: {nextInput}");
+
+                // }
+                // return (input, nextInput);
+
+                // resultInner.Add((input, nextInput));
+                if (lowest == null)
+                {
+                    lowest = nextInput;
+                }
+                else if (nextInput < lowest)
+                {
+                    lowest = nextInput;
+                }
+            }
+            // var lowestInner = resultInner.OrderBy(item => item.nextInput).First().nextInput;
+            Console.WriteLine($"Part2: {sv.start}: Lowest ___ {lowest}");
+            return lowest;
+        }).OrderBy(item => item);
+
+        Console.WriteLine($"Part2: Lowest is {result.First()}");
+
+
+        // part 2
+
     }
 
 
